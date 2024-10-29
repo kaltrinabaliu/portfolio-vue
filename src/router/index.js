79ceee391010433868c5a5d59d8fb3cb43939d4b
Kaltrinë Baliu <kaltrine.baliu@student.uni-pr.edu>
2024-10-29 02:19:48 +0100

    update

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  // Optionally add a redirect from any other path
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/',
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <NavBar></NavBar>
+      <HeroSection></HeroSection>
+      <About></About>
+      <Work></Work>
+      <Experience></Experience>
+      <MyEducation></MyEducation>
+      <ContactForm></ContactForm>
+      <Footer></Footer>
+  
+    </div>
+  </template>
+  
+  <script>
+  import About from "../components/About.vue";
+  import ContactForm from "../components/Contact.vue";
+  import Experience from "../components/Experience.vue";
+  import Footer from "../components/Footer.vue";
+  import HeroSection from "../components/HeroSection.vue";
+  import Work from "../components/Work.vue";
+  import NavBar from "../components/NavBar.vue";
+  import MyEducation from "../components/MyEducation.vue";
+  export default {
+    name: "App",
+    components: {
+      NavBar,
+      HeroSection,
+      About,
+      Work,
+      Experience,
+      MyEducation,
+      ContactForm,
+      Footer
+    },
+  };
+  </script>
+  
+  <style></style>
+  
